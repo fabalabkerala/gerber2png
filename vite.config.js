305145ef663b49278e1ff6c807be6c4c1dd4f920
Muhammed Midlaj N <midlaj4n@gmail.com>
2026-04-01 10:56:03 +0530

    minor update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/gerber2png/',
+export default defineConfig(() => {
+  const isGitHubPagesBuild = process.env.DEPLOY_TARGET === 'github-pages'
+
+  return {
+    plugins: [react()],
+    base: isGitHubPagesBuild ? '/gerber2png/' : '/',
+  }
 })
